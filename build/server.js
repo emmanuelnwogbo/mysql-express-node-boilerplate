e62c5bdeaef879c0427edd9295bd527271a3cfc4
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2018-10-22 01:30:43 +0100

    added Procfile

--- build/server.js
+++ build/server.js
@@ -1,9 +1,37 @@
 "use strict";
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _http = _interopRequireDefault(require("http"));
+
 var _express = _interopRequireDefault(require("express"));
 
-require("./src/test");
+var _bodyParser = _interopRequireDefault(require("body-parser"));
+
+var _cookieParser = _interopRequireDefault(require("cookie-parser"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-console.log(_express.default);
+const app = (0, _express.default)();
+
+const server = _http.default.createServer(app);
+
+const port = 3000;
+app.use(_bodyParser.default.json());
+app.use((0, _cookieParser.default)());
+app.get('/', (req, res) => {
+  console.log(req);
+  res.send('hello');
+});
+server.listen(port);
+server.on('error', function () {
+  console.log('error');
+});
+server.on('listening', function () {
+  console.log('listening');
+});
+var _default = app;
+exports.default = _default;
