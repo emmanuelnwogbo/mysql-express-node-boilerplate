75d21ae6f3966c2ef6a3b6751b72b37e6efa2995
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2018-10-22 14:17:04 +0100

    built for db config

--- build/server.js
+++ build/server.js
@@ -13,8 +13,32 @@ var _bodyParser = _interopRequireDefault(require("body-parser"));
 
 var _cookieParser = _interopRequireDefault(require("cookie-parser"));
 
+var _dotenv = _interopRequireDefault(require("dotenv"));
+
+var _mysql = _interopRequireDefault(require("mysql"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+_dotenv.default.config();
+
+const connection = _mysql.default.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+  connection.end(function () {
+    console.log('done');
+  });
+});
 const app = (0, _express.default)();
 
 const server = _http.default.createServer(app);
