9538f5fe2eed10613783e2bf1689af04fe1c1165
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2018-10-22 14:14:13 +0100

    added env and mysql db connections and connection logic

--- index.js
+++ index.js
@@ -2,7 +2,30 @@ import http from 'http';
 import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
 
+import mysql from 'mysql';
+
+dotenv.config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+  connection.end(function () {
+    console.log('done')
+  });
+});
 
 const app = express();
 const server = http.createServer(app);
